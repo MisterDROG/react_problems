9025f415eb35a47e4b9d5d9877df4290a24052b5
MrDrog <fireman3085@mail.ru>
2023-10-06 18:28:49 +0300

    new problem: books mapping problem

---
+++ src/fullApps/booksMapping.js
@@ -0,0 +1,69 @@
+
+
+const reviewsCollector = (
+    book: Book,
+    users: User[],
+    reviews: Review[]
+  ): ReviewInformation[] => {
+    return reviews
+      .filter((review) => book.reviewIds.find((id) => review.id === id))
+      .map((review) => ({
+        id: review.id,
+        user: users.find((user) => user.id === review.userId) || {
+          id: "sdf",
+          name: "No reviewer"
+        },
+        text: review.text
+      }));
+  };
+  
+  const toBookInformation = (
+    book: Book,
+    users: User[],
+    reviews: Review[]
+  ): BookInformation => {
+    return {
+      id: book.id,
+      name: book.name || "Книга без названия",
+      author: users.find((user) => user.id === book.authorId) || {
+        name: "Без автора",
+        id: "id"
+      },
+      reviews: reviewsCollector(book, users, reviews),
+      description: book.description
+    };
+  };
+  
+  const App: FC = () => {
+    const [books, setBooks] = useState<Book[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
+    const [reviews, setReviews] = useState<Review[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
+  
+    useEffect(() => {
+      const fetchBooks = async () => {
+        setIsLoading(true);
+        const fetchedBooks = await getBooks();
+        const fetchedUsers = await getUsers();
+        const fetchedReviews = await getReviews();
+        setBooks(fetchedBooks);
+        setUsers(fetchedUsers);
+        setReviews(fetchedReviews);
+        setIsLoading(false);
+      };
+      fetchBooks();
+    }, []);
+  
+    return (
+      <div>
+        <h1>Мои книги:</h1>
+        {isLoading && <div>Загрузка...</div>}
+        {!isLoading &&
+          books.map((b) => (
+            <Card key={b.id} book={toBookInformation(b, users, reviews)} />
+          ))}
+      </div>
+    );
+  };
+  
+  export default App;
